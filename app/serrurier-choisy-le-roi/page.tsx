b49679a15ade3288b5e6a1c867af28d313a0c8df
geuderiWebebsite <dsm94transport@gmail.com>
2025-11-14 16:49:49 +0100

    Update Google Maps iframe source for Choisy-le-Roi

--- app/serrurier-choisy-le-roi/page.tsx
+++ app/serrurier-choisy-le-roi/page.tsx
@@ -223,7 +223,7 @@ export default function SerrurierChoisyleRoi() {
               </div>
               <div className="w-full h-64 bg-gray-200 rounded-lg overflow-hidden">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20996.734257!2d2.4437314!3d48.8617037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66d8b6d0d0ca3%3A0x40b82c3688c9460!2sMontreuil!5e0!3m2!1sfr!2sfr!4v1234567890"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21039.70122573897!2d2.399581919017688!3d48.76350950309073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6749099be334b%3A0x40b82c3688b3960!2sChoisy-le-Roi!5e0!3m2!1sfr!2sfr!4v1763135371538!5m2!1sfr!2sfr"
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
