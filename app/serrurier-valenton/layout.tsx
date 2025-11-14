8dc94a00c8988b162cef93b3b763c8d25d6a4f82
geuderiWebebsite <dsm94transport@gmail.com>
2025-11-14 09:37:33 +0100

    Create layout.tsx

---
+++ app/serrurier-valenton/layout.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  robots: "noindex, nofollow",
+  title: "Serrurier Valenton - Intervention 24h/24",
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
