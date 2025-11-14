956f1944e01a557c25002206510b25a4f9b80b66
geuderiWebebsite <dsm94transport@gmail.com>
2025-11-14 16:52:18 +0100

    Update Google Maps embed source for Thiais

--- app/serrurier-thiais/page.tsx
+++ app/serrurier-thiais/page.tsx
@@ -223,7 +223,7 @@ export default function SerrurierThiais() {
               </div>
               <div className="w-full h-64 bg-gray-200 rounded-lg overflow-hidden">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20996.734257!2d2.4437314!3d48.8617037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66d8b6d0d0ca3%3A0x40b82c3688c9460!2sMontreuil!5e0!3m2!1sfr!2sfr!4v1234567890"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21040.963226413332!2d2.366319769008887!3d48.76049695476203!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6746c1d65f8d5%3A0x40b82c3688b37b0!2s94320%20Thiais!5e0!3m2!1sfr!2sfr!4v1763135519238!5m2!1sfr!2sfr"
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
