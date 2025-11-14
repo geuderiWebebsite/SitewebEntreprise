4a3067200c72a955bdf8f659896aef3c11b67436
geuderiWebebsite <dsm94transport@gmail.com>
2025-11-14 16:36:48 +0100

    Update Google Maps embed source for Clichy

--- app/serrurier-clichy/page.tsx
+++ app/serrurier-clichy/page.tsx
@@ -223,7 +223,7 @@ export default function SerrurierClichy() {
               </div>
               <div className="w-full h-64 bg-gray-200 rounded-lg overflow-hidden">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20997.934851!2d2.4553464!3d48.7778037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e60d0e3b3e7e27%3A0x40b82c3688c9460!2sCr%C3%A9teil!5e0!3m2!1sfr!2sfr!4v1234567890"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10490.367855443603!2d2.293830476009904!3d48.90411299604992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66f0c8459badf%3A0x705da473b0136bcb!2sClichy!5e0!3m2!1sfr!2sfr!4v1763134514888!5m2!1sfr!2sfr"
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
